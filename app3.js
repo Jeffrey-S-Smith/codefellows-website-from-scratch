996485e6bd9dd47a4501d3fe3dbaf1cf8d006aa1
Jeffrey Smith <jeffrey_s_smith@live.com>
2021-10-07 16:03:10 -0700

    updated

--- app3.js
+++ app3.js
@@ -1,7 +1,6 @@
 let userAnswer = prompt('Are you frustrated?');
-console.log('The answer to empire is: ' + userAnswer);
 
-if(userAnswer == 'yes'){
+if(userAnswer.toLowerCase() == 'yes'){
     let url = "https://st.depositphotos.com/1654249/1263/i/600/depositphotos_12630322-stock-photo-ashamed-3d-man-sitting-over.jpg";
     document.write('<img id="frustrated" src="' + url + '">');
-  
+}
