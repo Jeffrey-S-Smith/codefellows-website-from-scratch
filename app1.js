d9f64ce787fc9fab46cd0bf9bd3770ab8e14c7b2
Jeffrey Smith <jeffrey_s_smith@live.com>
2021-10-08 11:27:25 -0700

    Updated

--- app1.js
+++ app1.js
@@ -22,8 +22,12 @@ function joinFrustratedClub(){
     }
 
     if(userAnswer.toLowerCase() == 'yes'){
+      let numberPic = prompt("How many picture do you want to see!");
+      for (let i = 0; i<numberPic; i++) {
         let url = "https://st.depositphotos.com/1654249/1263/i/600/depositphotos_12630322-stock-photo-ashamed-3d-man-sitting-over.jpg";
         document.write('<img id="frustrated" src="' + url + '">');
+      }
+        
     }
 }
 
@@ -36,3 +40,19 @@ function findAMake(ford, chevolet, bently){
 findAMake('hyundai', 'kia', 'mercedes‑benz');
 findAMake('audi', 'jeep', 'toyoto');
 findAMake('buick', 'acura', 'bmw'); 
+
+//////////////////////////////////////////////////////////////////////////////////
+
+function howDoYouRate(){
+  let userRate = prompt('How do you rate your fustration, Rate your frustration 1 - 5?');
+  while (userAnswer.toLowerCase() != "yes") {
+    userAnswer = prompt('Hello, Do you want to join the Frustrated Club Yes is the only good answer?');
+  }
+
+  if(userAnswer.toLowerCase() == 'yes'){
+      let url = "https://st.depositphotos.com/1654249/1263/i/600/depositphotos_12630322-stock-photo-ashamed-3d-man-sitting-over.jpg";
+      document.write('<img id="frustrated" src="' + url + '">');
+  }
+}
+
+/*Get requirement 2 working and you can resubmit for full credit.*/
