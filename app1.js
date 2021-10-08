7fc6cf9315b6deeabf377009fa8ade8d3e366c2a
Jeffrey Smith <jeffrey_s_smith@live.com>
2021-10-07 17:44:45 -0700

    update

--- app1.js
+++ app1.js
@@ -1,1 +1,35 @@
 console.log("Happy Coding!");
+/////////////////////////////////////////////////////////////////////
+function getUserName(){
+    let userName = prompt('Please enter your name!');
+console.log(userName);
+
+if(userName == 'Jeffrey'){
+  document.write('<h2>Welcome Back '  + userName + '</h2>');
+} else {
+  document.write('<h2>Welcome to my site' + userName + '</h2>');
+}
+return userName;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+function joinFrustratedClub(){
+    let userAnswer = prompt('Hello, Do you want to join the Club?');
+    
+    if(userAnswer.toLowerCase() == 'yes'){
+        let url = "https://st.depositphotos.com/1654249/1263/i/600/depositphotos_12630322-stock-photo-ashamed-3d-man-sitting-over.jpg";
+        document.write('<img id="frustrated" src="' + url + '">');
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+function findAMake(ford, chevolet, bently){
+    console.log('Your Makes are ' + ford + ', ' + chevolet + ', ' + bently +'!');
+}
+
+findAMake('hyundai', 'kia', 'mercedes‑benz');
+findAMake('audi', 'jeep', 'toyoto');
+makePizza('buick', 'acura', 'bmw'); 
--- app2.js
+++ app2.js
@@ -2,7 +2,7 @@ let userName = prompt('Please enter your name!');
 console.log(userName);
 
 if(userName == 'Jeffrey'){
-  document.write('<h2>Welcome Lord ' + userName + '</h2>');
+  document.write('<h2>Welcome' + userName + '</h2>');
 } else {
   document.write('<h2>Welcome ' + userName + '</h2>');
 }
